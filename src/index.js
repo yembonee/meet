d813c14522a5b869c52c40cdb24e5b785f31d6b8
yembonee <yemsonidowu@gmail.com>
2023-12-14 15:02:20 -0800

    Updates

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 import * as atatus from "atatus-spa";
-atatus.config("85f1ec7dbc4845588cfb3370efe4bdb1").install();
+atatus.config("eab85a28bb7848feb772e48761911a52").install();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -24,3 +24,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+atatus.notify(new Error("Test Atatus Setup"));
