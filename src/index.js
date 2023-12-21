8d7af8ca4530c493262941cd72ab8a23a78c41f9
yembonee <yemsonidowu@gmail.com>
2023-12-21 13:02:40 -0800

    Final Index Atatus integration

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
-
 import * as atatus from "atatus-spa";
-atatus.config("eab85a28bb7848feb772e48761911a52").install();
+atatus.config("8934cbb1dd0146cfb523551793d436d5").install();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -24,4 +23,3 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-atatus.notify(new Error("Test Atatus Setup"));
