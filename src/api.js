148976e15b9d1cd2d6342894380238734936ae35
yembonee <yemsonidowu@gmail.com>
2023-12-29 18:13:41 -0800

    Updated api.js
    Added Offline Cached data to be allowed

--- src/api.js
+++ src/api.js
@@ -72,6 +72,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -83,6 +89,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
